662346198efd93afe35047a95f9bb14356bc60c7
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-08-26 16:43:57 +0300

    +

--- testing/main.cpp
+++ testing/main.cpp
@@ -1,14 +1,23 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
-int main() {
-
-	char str2[3];
-
-	str2[0] = 'b';
-	str2[1] = 'e';
-	str2[2] = 'e';
-
-	cout << str2;
-	return 0;
+void main()
+{
+	const int SIZE = 10;
+	int arr[SIZE] = { 24, 11, 22, 33, 44, 55, 66, 77, 88, 99 };
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr + i << " = " << arr[i] << " = " << *(arr + i) << endl;
+	}
+	/*cout << typeid(arr[3]).name() << endl;
+	cout << typeid('A').name() << endl;
+	cout << typeid(0.2).name() << endl;
+	string (name1) { "Anton hello!"};
+	string name2 = { "Irina hello!"};
+	cout << name1 << endl;
+	cout << name2 << endl;
+	cout << typeid(name1).name() << endl;
+	cout << typeid(name2).name() << endl;*/
+	
 }
